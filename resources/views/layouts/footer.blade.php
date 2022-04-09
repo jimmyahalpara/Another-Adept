f3dbab5a549f1a527cf56bb4445b135634fc85c1
jimmyahalpara <jimmyahalpara123@gmail.com>
2022-04-09 15:03:47 +0530

    custamized footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,20 +1,21 @@
 <footer class="pt-3 d-flex justify-content-center align-items-center flex-column">
     <div id="userfulLinks" class="row px-5 mx-5 mt-5">
         <div class="userFulLinksItem col-12 col-sm-6 col-lg-3" id="userfulLinkItem1">
-            <p class="no-margins smallText">Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit,
-                corporis.</p>
-            <p class="smallText">email@company.com</p>
+            <p class="no-margins smallText">
+                <img id="footer-logo" class="border border-light bg-light" src="{{ asset('assets/images/logo.svg') }}" alt="">
+            </p>
+            <p class="smallText">help@serviceadept.me</p>
             <p class="smallText">+1-111-111-1111</p>
         </div>
         <div class="userFulLinksItem col-12 col-sm-6 col-lg-3" id="userfulLinkItem2">
-            <p class="mediumBoldText">Shopping & Categories</p>
-            <p class="smallText"><a href="">Mens Shopping</a></p>
-            <p class="smallText"><a href="">Women Shopping</a></p>
-            <p class="smallText"><a href="">Kid's Shopping </a></p>
+            <p class="mediumBoldText">Service & Categories</p>
+            <p class="smallText"><a href="{{ route('search', ['categories_filter' => [2]]) }}">Handy Man</a></p>
+            <p class="smallText"><a href="{{ route('search', ['categories_filter' => [5]]) }}">Maid</a></p>
+            <p class="smallText"><a href="{{ route('search', ['categories_filter' => [7]]) }}">AC Service</a></p>
         </div>
         <div class="userFulLinksItem col-12 col-sm-6 col-lg-3" id="userfulLinkItem3">
             <p class="mediumBoldText">Useful Links</p>
-            <p class="smallText"><a href="">Homepage</a></p>
+            <p class="smallText"><a href="{{ route('home') }}">Homepage</a></p>
             <p class="smallText"><a href="">About Us</a></p>
             <p class="smallText"><a href="">Help</a></p>
             <p class="smallText"><a href="">Contact Us</a></p>
@@ -23,16 +24,18 @@
             <p class="mediumBoldText">Help & Information</p>
             <p class="smallText"><a href="">Help</a></p>
             <p class="smallText"><a href="">FAQ's</a></p>
-            <p class="smallText"><a href="">Shipping</a></p>
-            <p class="smallText"><a href="">Tracking ID</a></p>
-
+            <p class="smallText"><a href="">Terms and Condition</a></p>
         </div>
     </div>
     <div>
         <div id="sep"></div>
         <div class="copyrightContainer d-flex justify-content-center align-items-center flex-column">
             <p class="smallText no-margins">Copyright ©{{ date('Y') }} {{ config('app.name', 'Service Adept') }}. All Rights Reserved.</p>
-            <p class="smallText no-margins">Lorem, ipsum dolor.</p>
+            <p class="smallText no-margins">
+                <i class="fa-solid fa-quote-left"></i>
+                Lorem, ipsum dolor
+                <i class="fa-solid fa-quote-right"></i>
+            </p>
             <div class="copyrightSocialMediaIcons">
                 <i style="font-size:24px" class="fa"></i>
                 <i style="font-size:24px" class="fa"></i>
@@ -95,6 +98,11 @@
 
     .mediumBoldText {
         font-weight: bold;
+    }
+
+    #footer-logo {
+        width: 100%;
+
     }
 
 </style>
